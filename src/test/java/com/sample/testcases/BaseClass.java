d0ed075ca2d2a58e9a1d67ca5c1bab4557ca9d58
vikasini1234 <ivikasini@gmail.com>
2022-10-09 20:10:53 -0700

    new

--- src/test/java/com/sample/testcases/BaseClass.java
+++ src/test/java/com/sample/testcases/BaseClass.java
@@ -1,2 +1,68 @@
-package com.sample.testcases;public class BaseClass {
+package com.sample.testcases;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import com.sample.utlities.ReadConfig;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.Parameters;
+
+public class BaseClass {
+    ReadConfig readconfig = new ReadConfig();
+    public String baseURL = readconfig.getApplicationURL();
+    ;
+    public String username = readconfig.getUseremail();
+    public String password = readconfig.getPassword();
+    public static WebDriver driver;
+    public static Logger logger; //Added logger
+    @Parameters("browser")
+    @BeforeClass
+    public void setup(String br) {
+        //  System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"//drivers/chromedriver.exe" );
+        //   driver=new ChromeDriver();
+        logger = Logger.getLogger("SampleTestProjV"); //Added logger
+        PropertyConfigurator.configure("Log4j.properties");//Added logger
+        if (br.equals("firefox")) {
+            // Firefox Browser
+            System.setProperty("webdriver.gecko.driver", readconfig.getFirefoxPath());
+            driver = new FirefoxDriver();
+        } else if (br.equals("chrome")) {
+            // opens the browser
+            System.setProperty("webdriver.chrome.driver", readconfig.getChromePath());
+            driver = new ChromeDriver();
+        } else if (br.equals("edge")) {
+            // opens the browser
+            System.setProperty("webdriver.ie.driver", readconfig.getIEPath());
+            driver = new EdgeDriver();
+        }
+
+        // Global implicit Wait
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+    }
+
+
+
+   @AfterClass
+    public void tearDown()
+   {
+     driver.quit();
+    }
+    public void captureScreen(WebDriver driver, String tname) throws IOException, IOException {
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File source = ts.getScreenshotAs(OutputType.FILE);
+        File target = new File(System.getProperty("user.dir") + "/Screenshots/" + tname + ".png");
+        FileUtils.copyFile(source, target);
+        System.out.println("Screenshot taken");
+    }
 }
--- src/test/java/com/sample/testcases/TC_LoginTest_001.java
+++ src/test/java/com/sample/testcases/TC_LoginTest_001.java
@@ -1,2 +1,40 @@
-package com.sample.testcases;public class TC_LoginTest_001 {
+package com.sample.testcases;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import com.sample.pageobjectmadel.LoginPage;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+
+//import  com.sample.testcases.BaseClass;
+//import static com.sample.testcases.BaseClass.driver;
+
+public class TC_LoginTest_001 extends BaseClass {
+
+
+    @Test
+    public void loginTest() throws InterruptedException, IOException {
+        driver.get(baseURL);
+        logger.info("User provided"); //logger msg
+        LoginPage lp = new LoginPage(driver);
+        lp.setUserName(username);
+        lp.setPassword(password);
+        logger.info("Provided credentials"); //logger msg
+        lp.clickLogin();
+        logger.info("Click on login button"); //logger msg
+        logger.info("Verifying Title"); //logger msg
+        if (driver.getTitle().equals("Dashboard / nopCommerce administration")) {
+            Thread.sleep(5000);
+            lp.clickLogout();
+            Assert.assertTrue(true);
+            logger.info("login Passed"); //logger msg
+        } else {
+            captureScreen(driver,"loginTest");
+            Assert.assertTrue(false);
+            logger.info("login failed"); //logger msg
+        }
+
+    }
+
 }
